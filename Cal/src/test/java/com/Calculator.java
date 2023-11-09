6371b336fe289c699779c06b3709e6967d9c1d86
Pratibha <pratibha.mastud16@gmail.com>
2023-11-09 16:11:01 +0530

    Adding calculator project

---
+++ Cal/src/test/java/com/Calculator.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.testng.annotations.Test;
+
+public class Calculator {
+	@Test
+	public void sum()
+	{
+		int a=5;
+		int b=6;
+		int sum=a+b;
+		System.out.println("Addition of two number is:"+sum);
+	}
+
+}
+
+
+
+
